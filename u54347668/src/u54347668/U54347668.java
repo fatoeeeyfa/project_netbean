2cc2182698a09a820f92ff4ef6898e4795d73cfc
nu54347668 <fatoeeeyfa@hotmail.com>
2012-06-18 16:39:59 +0700

    new commit
    new commit

--- u54347668/src/u54347668/U54347668.java
+++ u54347668/src/u54347668/U54347668.java
@@ -15,6 +15,7 @@ public class U54347668 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello,mom");
-    }
+        System.out.println("Hello,World");
+        
+        }
 }
