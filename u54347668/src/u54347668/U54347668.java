256b4c5f029c25904a7c8b5da896876ca4e590f0
nu54347668 <fatoeeeyfa@hotmail.com>
2012-06-18 16:26:04 +0700

    new commit
    new commit

---
+++ u54347668/src/u54347668/U54347668.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u54347668;
+
+/**
+ *
+ * @author ITSTATION NU
+ */
+public class U54347668 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
